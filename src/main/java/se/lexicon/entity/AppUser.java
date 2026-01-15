f653979221f4c64f2ff63ac4239086c3beb3eebe
ZicoCoder <Azzoug.zackaria@gmail.com>
2026-01-15 08:23:14 +0100

    Reformating AppUser entity for improved readability and indentation consistency

--- src/main/java/se/lexicon/entity/AppUser.java
+++ src/main/java/se/lexicon/entity/AppUser.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
+
 @Getter
 @ToString
 @EqualsAndHashCode
@@ -12,22 +13,22 @@ import java.time.LocalDate;
 @Entity
 public class AppUser {
 
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-@Column(nullable = false, updatable = false)
-        private int id;
-        @Setter
-        @Column(length = 50, unique = true, nullable = false)
-        private String username;
-        @Setter
-         @Column(length = 50, nullable = false)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private int id;
+    @Setter
+    @Column(length = 50, unique = true, nullable = false)
+    private String username;
+    @Setter
+    @Column(length = 50, nullable = false)
 
-        private String password;
-        @Setter
-        private LocalDate regDate;
-@OneToOne
+    private String password;
+    @Setter
+    private LocalDate regDate;
+    @OneToOne
 //@JoinColumn(name = "details_id")
-        private Details userDetails;
+    private Details userDetails;
 
     public AppUser(String username, String password, LocalDate regDate, Details userDetails) {
 
